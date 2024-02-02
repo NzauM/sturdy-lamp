a0b99128f817688a8b47c43ff0cf74f64e13e666
NzauM <mercywaenu16@gmail.com>
2024-02-02 10:55:03 +0300

    add state and inverse data flow

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import { useState } from 'react';
 import Task from './Task.js'
 
 
 function App() {
+  const[secretWord, setsecretWord] = useState("")
   const myTasks = [
     {title: "Create React App", description: "Create react app using vite to understand the folder structure", dueDate: "2024-Feb-29th"},
     {title: "Create Flask App", description: "Create flask app using flask to understand the folder structure", dueDate: "2024-Mar-29th"},
     {title: "Create Python App", description: "Create python app using python3 to understand the folder structure", dueDate: "2024-Apr-29th"}
   ]
-  
+
+  const[tasks, setTasks] = useState(myTasks)
+
+
+  function receiveChildData(secret){
+    // receives data from child component
+    alert("Alas, I have been called by my child")
+    console.log(secret);
+    setsecretWord(secret)
+  }
   return (
    <>
    <h1>Welcome to my Todo App</h1>
-   {myTasks.map((task,index)=>{
+   <p>Secret Word is: {secretWord}</p>
+   {tasks.map((task,index)=>{
     return(
-      <Task key={index} title={task.title} description={task.description} dueDate={task.dueDate}/>
+      <Task key={index} title={task.title} description={task.description} dueDate={task.dueDate} parentFn={receiveChildData}/>
     )
    })}
    </>
--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,29 @@
-function Task({title, description, dueDate}){
+import { useState } from "react"
+
+function Task({title, description, dueDate, parentFn}){
+    const[buttonColor, setButtonColor] = useState("blue")
+    const[buttonText, setButtonText] = useState(`Due-Date:${dueDate}`)
+
+
+    function changeButton(e){
+      // setButtonColor((color)=>(color==="blue" ? "red" : "blue"))      
+      setButtonColor("red")
+      setButtonText("Task Completed")
+    }
+
+    let secretInfo = "Js is lowkey annoying"
+
+    function passInfoToPArent(){
+      // pass secret info to parent
+      console.log(parentFn)
+      parentFn(secretInfo)
+    }
     return(
         <>
         <h3>Task Title: {title} </h3>
         <p>Task Description: {description}</p>
-        <button>Due-Date:{dueDate} </button>
+        <button onClick={changeButton} style={{backgroundColor: buttonColor, color:"white"}}> {buttonText} </button>
+        <button onClick={passInfoToPArent}>Show Secret Word</button>
         </>
     )
 }
